b3a505e85ecd2daf6031ab413132f20c87896974
Teppo <vamteppo@protonmail.com>
2024-04-09 12:17:40 +0300

    get function universal

--- pi_scp.py
+++ pi_scp.py
@@ -4,18 +4,26 @@ Get market data from raspberry pi.
 import os
 import subprocess
 
-def get_volume():
+def get():
     """
-    Replaces local volume with volume stored in raspi.
+    Replaces local volume and orders with ones stored in raspi.
     """
     cwd = os.getcwd()
 
-    # Out with the old
+    # Remove old
     old_vols = os.listdir(cwd+"/market/volume")
     for vol in old_vols:
-        os.remove(cwd+"/market/volume/"+vol)
+        os.remove(cwd+"\\market\\volume\\"+vol)
+
+    old_ord = os.listdir(cwd+"/market/orders")
+    for ord in old_ord:
+        os.remove(cwd+"\\market\\orders\\"+ord)
     # Load to transfer folder
     subprocess.run(["scp","-r", "user@pi:/home/user/emi/market", cwd+"/transfer"])
     # Relocate if ok
     for db in os.listdir(cwd+"/transfer/market/volume"):
         os.replace(cwd+"\\transfer\\market\\volume\\"+db , cwd+"\\market\\volume\\"+db)
+    for db in os.listdir(cwd+"/transfer/market/orders"):
+        os.replace(cwd+"\\transfer\\market\\orders\\"+db , cwd+"\\market\\orders\\"+db)
+
+get()
