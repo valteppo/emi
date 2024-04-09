7a7ab37c35d4a910004672caa4b8b2c2a0888a9f
Teppo <vamteppo@protonmail.com>
2024-04-09 22:45:32 +0300

    import_export done

--- main.py
+++ main.py
@@ -15,32 +15,6 @@ Form item list to buy.
 Form eve market compliant string.
 """
 
-"""
-Operate with IDs, strings only for final translation.
-
-Proper market ID listing:
-    - esi_market.my_groups() are the desired groups.
-    - link groups to items.
-
-Get all lvl 4 agent systems and stations.
-
-Check buy/sell orders in effect in system:
-    - For buy, get the best nearest
-
-"""
-
-"""
-System data
-{"system_id" :  {"name": System name,
-                "constellation": System constellation ID,
-                "region": System region ID,
-                "security": System security,
-                "connections": [Connected system ID 1,
-                                ... ,
-                                Connected system ID n]
-                }
-}
-"""
 import janitor
 import time
 import datetime
