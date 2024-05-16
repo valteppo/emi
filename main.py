edefed2360f907c446d383273bca739821d93d8a
Teppo <vamteppo@protonmail.com>
2024-05-16 23:01:33 +0300

    output ei suodatu

--- main.py
+++ main.py
@@ -2,4 +2,24 @@
 """ 
 Main used for scripts.
 """
+import os
+import requests
+import datetime
+import tarfile
+
+cwd = os.getcwd()
+yesterday = datetime.datetime.now() - datetime.timedelta(days=14)
+print(yesterday)
+files = os.listdir(cwd + "/data/")
+for file in files:
+    tokens = file.split(".")
+    if tokens[-1] == "bz2":
+        tar = tarfile.open(cwd + "/data/" + file)
+        tar.extractall()
+        tar.close()
+
+# tar = tarfile.open(cwd+f"/data/")
+# tar.extractall(filter='data')
+# tar.close()
+
 
