9cff0b89aea544ebc50e16b5b5e20643b5930dd0
Teppo <vamteppo@protonmail.com>
2024-05-16 11:37:52 +0300

    done

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 
-""" Control loop
-Janitor:
-    Check folders exist.
-    Check sde exists.
-    Build system info database.
-Download orders.
-Determine best buy/sell locations in periphery regions.
-Get arbritrage candidates from price differences.
-Get market history for candidate items.
-Determine average volume.
-Determine hauling costs.
-Determine best items.
-Form item list to buy.
-Form eve market compliant string.
+""" 
+Main used for scripts.
 """
 
 import data_handling
